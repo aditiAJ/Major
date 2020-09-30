2a56d2eb3797ba39cc6725742029ac758b44146c
Aditi Jangid <aditijangid@Aditis-MacBook-Pro.local>
2020-09-30 22:12:36 +0530

    adding all files

---
+++ pro/frontend/startup/src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+const Footer = () => {
+    return ( <>
+     <footer>
+  <div className="line">
+    <div className="s-12 l-6">
+      <p>Copyright 2020
+      </p>
+    </div>
+    <div className="s-12 l-6">
+      <p className="right">
+        <a className="right" href="http://www.myresponsee.com" title="Responsee - lightweight responsive framework">All rights are reserved.</a>
+      </p>
+    </div>
+  </div>
+</footer>
+
+    </>);
+}
+ 
+export default Footer;
